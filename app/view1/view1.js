03f72ce677b33e8f1ccfdae4db7c0d3dffd817de
FKSI <francois.ky@axa-groupsolutions.com>
2016-12-06 11:12:19 +0100

    Add polling

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,22 +1,26 @@
-(function() {
-
 'use strict';
 
-angular
-  .module('myApp.view1', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {
-      templateUrl: 'view1/view1.html',
-      controller: 'View1Ctrl'
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', function($scope, $resource, poller) {
+
+    // Define your resource object.
+    var myResource = $resource("https://jsonplaceholder.typicode.com/posts/1", {});
+
+    // Get poller. This also starts/restarts poller.
+    var myPoller = poller.get(myResource);
+
+    // Update view. Since a promise can only be resolved or rejected once but we want
+    // to keep track of all requests, poller service uses the notifyCallback. By default
+    // poller only gets notified of success responses.
+    myPoller.promise.then(null, null, function(res){
+      console.log('🐼🐼🐼🐼🐼 -- Result from server : ', res)
     });
-  }])
-  .controller('View1Ctrl', View1Ctrl);
-
-  View1Ctrl.$inject = ['$scope'];
-
-  function View1Ctrl($scope) {
-
-  }
-
-
-})();
+});
